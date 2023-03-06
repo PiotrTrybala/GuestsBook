f2bed15de9174a4fe50abc5f3ad87cc0a9e9a362
Piotr Trybała <trybalapiotr4@gmail.com>
2023-03-06 15:35:24 +0100

    created structure for GuestView

---
+++ book_frontend/src/routes/GuestRow.svelte
@@ -0,0 +1,14 @@
+
+<script>
+    export let first = "";
+    export let last = "";
+    export let description = "";
+</script>
+
+<div class = "w-90 h-10 text-white bg-blue-500 flex justify-start items-center mb-2">
+
+    <p class = "inline-block mr-20 w-20 ml-2">{first}</p>
+    <p class = "inline-block mr-20 w-20">{last}</p>
+    <p class = "inline-block w-50">{description}</p>
+
+</div>
---
+++ book_frontend/src/routes/GuestView.svelte
@@ -0,0 +1,14 @@
+<script>
+    import GuestRow from "./GuestRow.svelte";
+
+</script>
+<div class = "w-100 h-96 flex justify-center items-center">
+    <div class = "w-90 h-64 overflow-hidden">
+        <GuestRow first="first" last="last" description="description"></GuestRow>
+        <GuestRow first="first" last="last" description="description"></GuestRow>
+        <GuestRow first="first" last="last" description="description"></GuestRow>
+        <GuestRow first="first" last="last" description="description"></GuestRow>
+        <GuestRow first="first" last="last" description="description"></GuestRow>
+    </div>
+    <!-- GuestRow -->
+</div>
