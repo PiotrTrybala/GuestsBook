fda93ac8f50b1692cb9f83df3e1a8484bba02782
Piotr Trybała <trybalapiotr4@gmail.com>
2023-03-06 15:35:38 +0100

    created structure for Admin login page

---
+++ book_frontend/src/routes/AdminLogin.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div class = "flex justify-center items-center w-100 h-96">
+    <form method="post" action="?/login" class="w-72 h-40 bg-purple-500 text-center box-border p-5">
+        <input type="text" placeholder="Username" class = "focus:outline-none"/><br/><br/>
+        <input type="password" placeholder="Password" class = "focus:outline-none"/><br/><br/>
+        <input type="submit" value="Sign in" class = "bg-white text-center h-8 w-40"/>
+    </form>
+</div>
