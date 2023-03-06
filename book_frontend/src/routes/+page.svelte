993053b71ee6fc0a1759262b53460aa13cd3f882
Piotr Trybała <trybalapiotr4@gmail.com>
2023-03-06 14:09:56 +0100

    create basic HTML structure for panel page

--- book_frontend/src/routes/+page.svelte
+++ book_frontend/src/routes/+page.svelte
@@ -1,9 +1,36 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
 
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
-</style>
+<div>
+
+    <div>
+
+        <div>
+            Navigation
+            <button>Guest form</button>
+            <button>Last n guests</button>
+            <button>Admin</button>
+        </div>
+
+        <div>
+            <div>FirstName LastName Description(0:30)</div>
+        </div>
+
+        <div>
+            <form method = "post" action="?/login">
+                <input type = "text" placeholder = "Username">
+                <input type = "password" placeholder = "Password">
+                <input type = "submit" value = "Sign in">
+            </form>
+        </div>
+
+        <form method="post">
+            <input name = "first_name" type = "text" placeholder="Imię">
+            <input name = "last_name" type = "text" placeholder="Nazwisko">
+            <textarea name = "description" rows="10" cols="30">
+                Description 
+            </textarea>
+            <input type = "submit" value = "Zostań gościem!">
+        </form>
+
+    </div>
+
+</div>
